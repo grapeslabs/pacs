1fe7b8ff39382bf247cddc881beb076d551d66d6
root <root@twl.su>
2026-05-04 11:16:18 +0300

    fix: start.sh

--- start.sh
+++ start.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-
 ENV_FILE=".env"
 LOG_PREFIX="[DEPLOY]"
-
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
-
 log() { echo -e "${GREEN}${LOG_PREFIX} $1${NC}"; }
 warn() { echo -e "${YELLOW}${LOG_PREFIX} WARNING: $1${NC}"; }
 error() { echo -e "${RED}${LOG_PREFIX} ERROR: $1${NC}"; exit 1; }
-
-if ! command -v docker compose &> /dev/null; then
-    error "Docker Compose not installed"
+if docker compose version &> /dev/null; then
+    DOCKER_COMPOSE="docker compose"
+elif command -v docker-compose &> /dev/null; then
+    DOCKER_COMPOSE="docker-compose"
+else
+    error "Docker Compose not installed. Please install docker-compose or docker compose plugin."
 fi
 
 if [ ! -f "$ENV_FILE" ]; then
@@ -25,6 +25,7 @@ source "$ENV_FILE"
 set +a
 
 log "Загрузка конфигурации из $ENV_FILE..."
+
 is_enabled() {
     local var_name=$1
     local value=${!var_name}
@@ -34,8 +35,9 @@ is_enabled() {
         return 1
     fi
 }
+
 log "Starting app..."
-if docker compose up -d app; then
+if $DOCKER_COMPOSE up -d app; then
     log "App started."
 else
     error "Starting failed"
@@ -43,10 +45,10 @@ fi
 
 if is_enabled "MEDIA_SERVER_ENABLED"; then
     log "Starting media-server..."
-    docker compose up -d media-server || warn "FAILED"
+    $DOCKER_COMPOSE up -d media-server || warn "FAILED"
 fi
 
 if is_enabled "ANALYTIC_ENABLED"; then
     log "Starting analytic services..."
-    docker compose up -d analytic-database analytic-api analytic-server media-server || warn "FAILED"
+    $DOCKER_COMPOSE up -d analytic-database analytic-api analytic-server media-server || warn "FAILED"
 fi
