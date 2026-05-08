c24b280e36f16b7651e12e3cac1b1a902d23f4f9
root <root@twl.su>
2026-05-08 13:42:53 +0300

    Fix: person table photo column type, php vars

---
+++ database/migrations/2026_05_08_134059_change_photo_to_person_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('person', function (Blueprint $table) {
+            $table->text('photo')->change();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('person', function (Blueprint $table) {
+           $table->string('photo')->change();
+        });    }
+};
