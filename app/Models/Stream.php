352969eaec09c1fe3213744dcf0b3da9a3ffb1a7
root <root@twl.su>
2026-05-27 20:18:25 +0300

    Fix: stream model

--- app/Models/Stream.php
+++ app/Models/Stream.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Stream extends Model
 {
-    use HasFactory, , SoftDeletes;
+    use HasFactory, SoftDeletes;
     protected $connection='pgsql';
     protected $table = 'streams';
     protected $fillable = [
